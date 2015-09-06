fd99ee5c314fa38fde261613b8b8fbf6a3bf16be
Marcel Klehr <mklehr@gmx.net>
2015-09-06 13:33:35 +0200

    Configure ckeditor toolbar

--- client.js
+++ client.js
@@ -13,6 +13,28 @@ function setup(plugin, imports, register) {
   document.body.appendChild(script)
 
   editor.registerEditor('html', function*() {
+    // configure ckeditor
+    CKEDITOR.editorConfig = function( config ) {
+      config.toolbarGroups = [
+        { name: 'clipboard', groups: [ 'clipboard', 'undo' ] },
+        { name: 'editing', groups: [ 'find', 'selection', 'spellchecker', 'editing' ] },
+        { name: 'links', groups: [ 'links' ] },
+        { name: 'insert', groups: [ 'insert' ] },
+        { name: 'forms', groups: [ 'forms' ] },
+        { name: 'tools', groups: [ 'tools' ] },
+        { name: 'document', groups: [ 'mode', 'document', 'doctools' ] },
+        { name: 'others', groups: [ 'others' ] },
+        '/',
+        { name: 'basicstyles', groups: [ 'basicstyles', 'cleanup' ] },
+        { name: 'paragraph', groups: [ 'list', 'indent', 'blocks', 'align', 'bidi', 'paragraph' ] },
+        { name: 'styles', groups: [ 'styles' ] },
+        { name: 'colors', groups: [ 'colors' ] },
+        { name: 'about', groups: [ 'about' ] }
+      ];
+
+      config.removeButtons = 'Underline,Subscript,Superscript,Undo,Redo,Source,About';
+    };
+
     // Replace textarea
     var textarea = document.createElement('textarea')
     textarea.setAttribute('id', 'doc')
