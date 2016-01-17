9ac75a254a6386974f27b106b7083109e1b55a80
Marcel Klehr <mklehr@gmx.net>
2016-01-17 20:53:31 +0100

    Implement export

--- index.js
+++ index.js
@@ -1,17 +1,24 @@
 var path = require('path')
   , domOT = require('dom-ot')
+  , vdomToHtml = require('vdom-to-html')
 
 module.exports = setup
-module.exports.consumes = ['ui', 'ot']
+module.exports.consumes = ['ui', 'ot', 'importexport']
 
 function setup(plugin, imports, register) {
   var ui = imports.ui
   var ot = imports.ot
+  var importexport = imports.importexport
 
   ui.registerModule(path.join(__dirname, 'client.js'))
   ui.registerStaticDir(path.join(__dirname, 'ckeditor'))
 
   ot.registerOTType('text/html', domOT)
+
+  importexport.registerExportProvider('text/html', 'text/html'
+  , function*(document, snapshot) {
+    return vdomToHtml(JSON.parse(snapshot.contents))
+  })
 
   register()
 }
